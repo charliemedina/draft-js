9f1b0457c47210a3e94a0923db969c10b664106f
charliemedina <charliemedina1989@gmail.com>
2019-05-31 11:35:23 +0200

    RichEditorExample created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import PageContainer from './components/PageContainer'
+import RichEditorExample from './components/RichEditorExample';
 
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <PageContainer />
+        <RichEditorExample />
       </div>
     );
   }
